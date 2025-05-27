8b71afacd0999e99184084ae6c3a0019e043830e
lilyanparma <lilyanparma@gmail.com>
2025-05-27 16:33:03 +0100

    Update main.js

--- main.js
+++ main.js
@@ -11,15 +11,4 @@ function processSale(product, finalPrice, paymentMethod, discountInfo = null) {
     
     // ... أي معالجة إضافية بعد إنشاء الفاتورة
 }
-
-// مثال استخدام:
-const productExample = {
-    name: "شوكولاتة داكنة 100جم",
-    price: 25.99
-};
-
-processSale(productExample, 23.39, 'cash', {
-    type: 'percent',
-    value: 10,
-    reason: 'خصم موسمي'
 });
