8985e7e40561d1edc35536022e56ad26bbe61925
lilyanparma <lilyanparma@gmail.com>
2025-05-27 18:18:22 +0100

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,22 @@
+// firebase-config.js
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDRtJBPOjpfXXfsaDo98E_NTT7KWzz8gj4",
+  authDomain: "pharmacy-system-9def6.firebaseapp.com",
+  databaseURL: "https://pharmacy-system-9def6-default-rtdb.firebaseio.com",
+  projectId: "pharmacy-system-9def6",
+  storageBucket: "pharmacy-system-9def6.firebasestorage.app",
+  messagingSenderId: "630855008414",
+  appId: "1:630855008414:web:b464bcb81c8a2091d0fb8f",
+  measurementId: "G-LC5NDKP6QZ"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const auth = getAuth(app);
+
+export { db, auth };
